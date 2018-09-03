95a9471979df503dd4c6051dd07358d946151b2a
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:02:46 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+       ?>
+    </p>
+  </body>
+</html>
